28ba6b2ef2232a645faf1207057f5f4740386e63
Ravi Choudhary <89929494+M20AIE288@users.noreply.github.com>
2023-09-25 19:30:28 +0530

    Add files via upload
    few changes

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI()
-    #llm = HuggingFaceHub(repo_id="starmpcc/Asclepius-13B", model_kwargs={"temperature":0.5, "max_length":512})
+    #llm = HuggingFaceHub(repo_id="tiiuae/falcon-180B-chat", model_kwargs={"temperature":0.5, "max_length":512})
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
